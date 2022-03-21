8c73709e79a51f5dd860fe8ea72df02c3e327bc0
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 15:08:03 -0400

    March 21, 2022 3:08 PM

--- w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
+++ w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
@@ -3,11 +3,11 @@ import Content from '../Content'
 
 var contentArray = [
     {title: 'Series Summary', content: "Led by Vince, an actor deemed the next big thing, four buddies migrate west from Queens, N.Y. Usually within his reach are his less successful half brother, Drama; Vince's best friend, Eric; and their irrepressible pal, Turtle. As the foursome adjusts to Tinseltown's fast pace and frantic deal-making, Vince's take-charge agent, Ari, exerts his influence -- often to Eric's annoyance.", index:1},
-    <br></br>,
+    
     {title: "Cast and Crew", content: <img src='/img/kanye.jpeg' alt='Guest Star Kanye West and Cast' height='300px' />, index:2},
-    <br></br>,
+    
     {title: "Celeberty Cameo Appearances", content:"Over the course of the 8 seasons, Entourage hired on celebrity cameo apperances on the show. Those name include Mark Walburg (the man behind the show), Kanye West, Sasha Grey, Bob Saget, Mike Tyson, and Jessica Simpson. Just to name a few", index:3},
-    <br></br>,
+    
     {title: "Rotten Tomatoes", content: <img src='/img/rottentom.jpeg' alt='Rotten Tomatoes Review' height='300px' />, index:4},
     
 ]
@@ -16,11 +16,11 @@ export default class CreateContent extends React.Component {
      
    render() {
        var element = contentArray.map(post =>
-           <post
+           <Content
                key={post.index}
                title={post.title}
                content={post.content}>
-           </post>
+           </Content>
        )       
        return (
             <>
