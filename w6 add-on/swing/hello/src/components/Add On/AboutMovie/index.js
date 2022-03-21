5b3d10209c7fabc74189e18f63086de13ec5a8f8
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 12:30:38 -0400

    March 21, 2022 12:30 PM

--- w6 add-on/swing/hello/src/components/Add On/AboutMovie/index.js
+++ w6 add-on/swing/hello/src/components/Add On/AboutMovie/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default class Example extends React.Component {
+export default class AboutMovie extends React.Component {
     render() {
-        return <p>My Favorite show is {this.props.show}, and my favorite character is from it is {this.props.character}</p>
+        return (
+        <>
+             <h1>My Favorite show is...</h1> 
+             <h2>{this.props.data.show}</h2>   
+             <img src="/img.Entourage.jpeg" alt = "Entourge CREW" width="75%"></img>
+             <p>{this.props.data.show} is an American comedy-drama television series that premiered on {this.props.data.year} on HBO with {this.props.data.seasons} seasons.</p>
+        )    <p>My Favorite favorite character from Entourage is {this.props.data.character}</p>
+        </>
+        )
     }
 }
