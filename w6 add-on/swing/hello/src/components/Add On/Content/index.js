4e3d60067ff40f9b16bb0a423724e4fca41144e4
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 13:28:06 -0400

    March 21, 2022 1:28 PM

---
+++ w6 add-on/swing/hello/src/components/Add On/Content/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
+    render(){
+        return (
+            <>
+                 <h3>{this.props.title}</h3>
+                 <div>{this.props.content}</div>
+                 <p><button onClick={() => {this.setstate({counter: this.state.counter + 1})}}>Likes: {this.state.counter}</button></p>
+            </>     
+        )
+    }
+}
