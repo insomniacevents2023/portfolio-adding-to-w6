c54393f9fe936e4024c93f695a968fc151f73736
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 14:05:31 -0400

    March 21, 2022 2:05 PM

--- w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
+++ w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Content from '../Content'
 
 var contentArray = [
-    {title: 'Hard Times', content: "The economy stinks, bees are dying, and movies are pretty much all sequels now. -Schmidt", index:1},
+    {title: '', content: "The economy stinks, bees are dying, and movies are pretty much all sequels now. -Schmidt", index:1},
     <br></br>,
     {title: "Dance Party", content: <img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:3},
     <br></br>,
     {title: "Self Care", content:"I am not a successful adult. I don't eat vegetables and/or take care of myself. -Nick Miller", index:2},
     <br></br>,
-    {title: "Dance Party", content: <img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:4},
+    {title: "Rotten Tomatoes", content: <img src='/img/dancing.gif' alt='Rotten Tomatoes Review' height='300px' />, index:4},
     
 ]
 
