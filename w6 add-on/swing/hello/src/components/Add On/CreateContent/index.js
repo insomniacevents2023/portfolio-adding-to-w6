ce0537093bc7296b34acde056763c219816bbcda
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 13:43:39 -0400

    March 21, 2022 1:43 PM

--- w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
+++ w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
@@ -4,3 +4,22 @@ import Content from '../Content'
 var contentArray = [
     {}
 ]
+
+export default class CreateContent extends React.Component {
+     
+   render() {
+       var element = contentArray.map(post =>
+           <Content
+               key={post.index}
+               title={post.title}
+               content={post.content}>
+           </Content>
+       )       
+       return (
+            <>
+            <h1>MORE CONTENT</h1>
+            {element}
+            </>
+       )
+    }
+}           
