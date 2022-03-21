ce072b24d1cfdd97f31f306976e7f347c099c211
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 15:21:20 -0400

    March 21, 2022 3:21 PM

--- w6 add-on/swing/hello/src/components/Add On/Content/index.js
+++ w6 add-on/swing/hello/src/components/Add On/Content/index.js
@@ -20,3 +20,22 @@ export default class Content extends React.Component {
         )
     }
 }
+export default class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+            
+        };
+    }
+    render(){
+        return (
+            <>
+                 <h3>{this.props.title}</h3>
+                 <div>{this.props.content}</div>
+                 <p><button onClick={() => {this.setState({counter: this.state.counter + 1})}}>LIKE ME: {this.state.counter}</button></p>
+                 
+            </>     
+        )
+    }
+}
