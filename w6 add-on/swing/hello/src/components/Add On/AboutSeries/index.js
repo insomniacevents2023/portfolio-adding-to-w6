fb9fc6c773fdc77b6c7b5ba6c6dddd774e214df0
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 14:49:43 -0400

    March 21, 2022 2:49 PM

--- w6 add-on/swing/hello/src/components/Add On/AboutSeries/index.js
+++ w6 add-on/swing/hello/src/components/Add On/AboutSeries/index.js
@@ -6,10 +6,10 @@ export default class AboutSeries extends React.Component {
         <>
              <h1>My Favorite television series is...</h1> 
              <h2>{this.props.data.series}</h2>   
-             <img src="/img.Entourage.jpeg" alt = "Entourge CREW" width="75%"></img><br></br>
+             <img src="/img/entourage.jpeg" alt = "Entourage CREW" width="75%"></img><br></br>
              <p>{this.props.data.series} is an American comedy-drama television series that premiered in {this.props.data.year} on HBO with {this.props.data.seasons} seasons.</p><br></br>
             <p>My Favorite favorite character from Entourage is {this.props.data.character}</p><br></br>
-            <img src="/img.Entourage.jpeg" alt = "Entourge CREW" width="75%"></img><br></br>
+            <img src="/img/turtle.jpeg" alt = "Turtle" width="75%"></img><br></br>
         </>
         )
     }
--- w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
+++ w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
@@ -4,11 +4,11 @@ import Content from '../Content'
 var contentArray = [
     {title: 'Series Summary', content: "Led by Vince, an actor deemed the next big thing, four buddies migrate west from Queens, N.Y. Usually within his reach are his less successful half brother, Drama; Vince's best friend, Eric; and their irrepressible pal, Turtle. As the foursome adjusts to Tinseltown's fast pace and frantic deal-making, Vince's take-charge agent, Ari, exerts his influence -- often to Eric's annoyance.", index:1},
     <br></br>,
-    {title: "Cast and Crew", content: <img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:2},
+    {title: "Cast and Crew", content: <img src='/img/kanye.jpeg' alt='Guest Star Kanye West and Cast' height='300px' />, index:2},
     <br></br>,
     {title: "Celeberty Cameo Appearances", content:"Over the course of the 8 seasons, Entourage hired on celebrity cameo apperances on the show. Those name include Mark Walburg (the man behind the show), Kanye West, Sasha Grey, Bob Saget, Mike Tyson, and Jessica Simpson. Just to name a few", index:3},
     <br></br>,
-    {title: "Rotten Tomatoes", content: <img src='/img/dancing.gif' alt='Rotten Tomatoes Review' height='300px' />, index:4},
+    {title: "Rotten Tomatoes", content: <img src='/img/rottentom.jpeg' alt='Rotten Tomatoes Review' height='300px' />, index:4},
     
 ]
 
