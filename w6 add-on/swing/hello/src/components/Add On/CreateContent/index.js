7d6b6fbcc5c6a14fdcd5c663aa7fac07e177034f
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 14:19:46 -0400

    March 21, 2022 2:19 PM

--- w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
+++ w6 add-on/swing/hello/src/components/Add On/CreateContent/index.js
@@ -4,9 +4,9 @@ import Content from '../Content'
 var contentArray = [
     {title: 'Series Summary', content: "Led by Vince, an actor deemed the next big thing, four buddies migrate west from Queens, N.Y. Usually within his reach are his less successful half brother, Drama; Vince's best friend, Eric; and their irrepressible pal, Turtle. As the foursome adjusts to Tinseltown's fast pace and frantic deal-making, Vince's take-charge agent, Ari, exerts his influence -- often to Eric's annoyance.", index:1},
     <br></br>,
-    {title: "Dance Party", content: <img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:3},
+    {title: "Cast and Crew", content: <img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:2},
     <br></br>,
-    {title: "Self Care", content:"I am not a successful adult. I don't eat vegetables and/or take care of myself. -Nick Miller", index:2},
+    {title: "Celeberty Guest Appearances", content:"Over the course of the 8 seasons Entourage took on, many celeberties had guest apperances on the show. Those name include Mark Walburg (Entoruage was made after him), Sasha Grey, Bob Saget, Mike Tyson, and Jessica Simpson", index:3},
     <br></br>,
     {title: "Rotten Tomatoes", content: <img src='/img/dancing.gif' alt='Rotten Tomatoes Review' height='300px' />, index:4},
     
