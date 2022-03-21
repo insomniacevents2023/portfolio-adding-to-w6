6311995f22cf668ab10959078e9f72f638f3ad36
insomniacevents2023 <97802776+insomniacevents2023@users.noreply.github.com>
2022-03-21 15:02:49 -0400

    March 21, 2022 3:02 PM

--- w6 add-on/swing/hello/src/components/Add On/Content/index.js
+++ w6 add-on/swing/hello/src/components/Add On/Content/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export default class Post extends React.Component {
+export default class Content extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
